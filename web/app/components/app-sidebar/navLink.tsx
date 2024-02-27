4b67ad743d8cdfe753703273a434bf903c4a7ee7
Jeremy <jerycohe05@gmail.com>
2024-02-27 14:53:13 -0500

    Updates

--- web/app/components/app-sidebar/navLink.tsx
+++ web/app/components/app-sidebar/navLink.tsx
@@ -44,7 +44,7 @@ export default function NavLink({
       key={name}
       href={href}
       className={classNames(
-        isActive ? 'bg-primary-50 text-primary-600 font-semibold' : 'text-gray-700 hover:bg-gray-100 hover:text-gray-700',
+        isActive ? 'bg-primary-50 text-gray-500 font-semibold' : 'text-gray-700 hover:bg-gray-100 hover:text-gray-700',
         'group flex items-center h-9 rounded-md py-2 text-sm font-normal',
         mode === 'expand' ? 'px-3' : 'px-2.5',
       )}
@@ -52,7 +52,7 @@ export default function NavLink({
       <NavIcon
         className={classNames(
           'h-4 w-4 flex-shrink-0',
-          isActive ? 'text-primary-600' : 'text-gray-700',
+          isActive ? 'text-gray-600' : 'text-gray-700',
           mode === 'expand' ? 'mr-2' : 'mr-0',
         )}
         aria-hidden="true"
